005d9250cca6f6ec7ec86b100b0e589bdcb780dc
jtsyganov <jtsyganov@gmail.com>
2023-01-18 21:04:48 +0200

    HW9

---
+++ HomeWork4.java
@@ -0,0 +1,17 @@
+package class9HomeWork;
+
+public class HomeWork4 {
+    public static void main(String[] args) {
+       // 4) Create an array that can store names of cars and store 6 elements into it.
+        // Print all values from the array.
+
+
+        String [] carNames={"Toyota","BMW","Porsche","Ferrari","Mercedes","Nissan"};
+        for (int i = 0; i < carNames.length; i++) {
+            System.out.println(carNames[i]);
+
+        }
+
+
+    }
+}
